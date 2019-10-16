99a795399f4f6919d91eba10cd09abc32e265c40
Stephen Fu <stephen.frx@gmail.com>
2019-10-16 16:08:33 +0800

    init project

---
+++ database/migrations/2019_09_29_082413_create_databases_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDatabasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('databases', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 255);
+            $table->string('slug', 255);
+            $table->text('remote_url')->nullable();
+            $table->text('local_url')->nullable();
+            $table->text('brief')->nullable();
+            $table->text('content')->nullable();
+            $table->text('links')->nullable();
+            $table->integer('status')->default(0)->comment('数据库状态：0-试用，1-正式购买，2-开放资源');
+            $table->unsignedBigInteger('user_id');
+            $table->text('remark')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('databases');
+    }
+}
---
+++ database/migrations/2019_09_29_082554_create_files_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 64);
+            $table->string('path', 128);
+            $table->string('type', 64)->nullable();
+            $table->unsignedBigInteger('database_id');
+            $table->timestamps();
+
+            $table->foreign('database_id')->references('id')->on('databases')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+}
