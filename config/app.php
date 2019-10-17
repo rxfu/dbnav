d8bf6915d04701455b2878f20348eff69effe446
Stephen Fu <stephen.frx@gmail.com>
2019-10-17 10:43:39 +0800

    增加中文翻译内容

--- config/app.php
+++ config/app.php
@@ -226,6 +226,12 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        /**
+         * Custom Service Facades...
+         */
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+        'Flash' => Laracasts\Flash\Flash::class,
     ],
 
 ];
