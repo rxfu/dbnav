65b7033f57767f99661194ec88550e536aa83ac9
Stephen Fu <stephen.frx@gmail.com>
2019-11-28 23:37:11 +0800

    修改数据库表结构

--- database/migrations/2019_09_29_082413_create_databases_table.php
+++ database/migrations/2019_09_29_082413_create_databases_table.php
@@ -20,7 +20,7 @@ class CreateDatabasesTable extends Migration
             $table->text('remote_url')->nullable();
             $table->text('local_url')->nullable();
             $table->text('brief')->nullable();
-            $table->text('content')->nullable();
+            $table->longText('content')->nullable();
             $table->integer('status')->default(0)->comment('数据库状态：0-试用，1-正式购买，2-开放资源');
             $table->date('expired_at')->nullable();
             $table->text('remark')->nullable();
